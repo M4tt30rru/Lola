d722f9e044a67b1a09df9695c3bb15e070804c9b
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-05-27 14:43:43 +0300

    if1 and if2 tests

--- src/test/java/il/ac/technion/cs/ssdl/lola/testcase/mathematica/If.java
+++ src/test/java/il/ac/technion/cs/ssdl/lola/testcase/mathematica/If.java
@@ -14,13 +14,33 @@ public class If {
 	 * 
 	 */
 			
+	// If[true,1,2] 
+	
+//	"" //
+//	+ "##Find do[##Any(_e),{##Identifier(_i),##Literal(_max)}];\n" //
+//	+ " ##replace for(int ##(_i)=0;##(_i)<##(_max);++##(_i)) {##(_e);}\n" //
+//	+ "do[f(i);g(i * 2),{i,100}];", //
+//"for(int i=0;i<100;++i) {f(i);g(i * 2);}");
+	
 	
 	@Test
 	public void if1() {
-		
-		String s = null;
-		String result = null;
+		String s = "" // 
+							 + "##Find if[##Literal(_condition),##Literal(_t),##Literal(_f)];\n " //
+							 + "	##replace if(##(_condition)) return ##(_t) else return ##(_f);\n"
+							 + "if[3,1,2];"; 
+		String result = "if(3)  return  1  else  return  2;\n";
 		auxz.runStringTest(s, result);
-		
+	}
+	
+	@Test
+	public void if2() {
+		String s = ""
+							 + "##Find(##BooleanLiteral) ##Either true ##or false" // 
+							 + "##Find if[##BooleanLiteral(_condition),##Literal(_t),##Literal(_f)];\n " //
+							 + "	##replace if(##(_condition)) return ##(_t) else return ##(_f);\n"
+							 + "if[true,1,2];"; 
+		String result = "if(true)  return  1  else  return  2;\n";
+		auxz.runStringTest(s, result);
 	}
 }
