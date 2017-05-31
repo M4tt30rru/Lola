b725160c05811c9a1b5510bf5470bd4d55fcad50
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-05-31 12:31:44 +0300

    nest update

--- src/test/java/il/ac/technion/cs/ssdl/lola/testcase/mathematica/Nest.java
+++ src/test/java/il/ac/technion/cs/ssdl/lola/testcase/mathematica/Nest.java
@@ -20,15 +20,54 @@ public class Nest {
 				 + "	##Match ##Any ##exceptFor ##Any, ##Any\n" //
 				 + "##Find(Expression)\n" //
 				 + "	##NoCommasExpression\n" //
-				 + "##Find Nest[##Any(_f),##Expression(_expr),##Literal(_n)];\n"
+				 + "##Find Nest[##Identifier(_f), ##Identifier(_expr) , ##Literal(_n)];\n"
 				 + "	##run {\n"
-				 + "fs = [str(_f)+'(' for _f in range(##(_n))]\n"
+				 + "_fs = str(_expr) # (_f.name)\n"
+//				 + "fs = [str(##(_f))+'(' for _f in range(##(_n))]\n"
 				 + "}\n" //
-				 + "	##replace ##(fs);\n"
+				 + "	##replace ##(_f);"
 //				 + "		##ForEach if(##(_t)>0) return ##(_t) else return -##(_t);\n"
 				 + "Nest[f,x,3];"; 
-		System.out.println(s);
-		String result = "f(f(f(x)));\n";
+//		System.out.println(s);
+		String result = "f(f(f(x)));";
 		auxz.runStringTest(s, result);
 	}
+
+	@Test
+	public void nest2() {
+		String s = ""
+				 + "##Find Nest[##Identifier(_f), ##Identifier(_x) , ##Literal(_n)];\n"
+				 + "	##replace ##(_f);"
+				 + "Nest[f,x,3];"; 
+		String result = "f(f(f(x)));";
+		auxz.runStringTest(s, result);
+	}
+	
+	@Test
+	public void nest3() {
+		String s = ""
+				 + "##Find Nest[##Identifier(_f)];\n"
+				 + "	##replace ##(_f)(x);"
+				 + "Nest[f];"; 
+		String result = "f(x);";
+		auxz.runStringTest(s, result);
+	}
+	
+	@Test
+	public void nest4() {
+		String s = ""
+				 + "##Find if[##Literal(_n)];\n"
+				 + "	##replace ##(_n);"
+				 + "if[3];"; 
+		String result = "3;";
+		auxz.runStringTest(s, result);
+	}
+
+}
+
+
+class Foo {
+	
+	
+	
 }
