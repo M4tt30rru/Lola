571af23f1d570105eb3538189bd636135d408a59
Matteo Orru' <matteo.orru@cs.technion.ac.il>
2017-05-31 21:18:06 +0300

    start working on ForAll

---
+++ src/test/java/il/ac/technion/cs/ssdl/lola/testcase/mathematica/ForAll.java
@@ -0,0 +1,20 @@
+package il.ac.technion.cs.ssdl.lola.testcase.mathematica;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ForAll {
+	
+//	ForAll[x,expr]
+//			represents the statement that expr is True for all values of x.
+//			ForAll[x,cond,expr]
+//			states that expr is True for all x satisfying the condition cond.
+//			ForAll[{x1,x2,…},expr]
+//			states that expr is True for all values of all the xi.
+	
+	@Test
+	public void forAll1() {
+		fail("Not yet implemented");
+	}
+}
